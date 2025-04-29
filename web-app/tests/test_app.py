dc7828987f35d7a2435352d727d957e4afeed143
lanadavydov <lanadavy03@gmail.com>
2025-04-28 23:18:10 -0400

    Add web app badge to readme and create mongoDB workflow

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,1 +1,8 @@
-# testing file for web app
+import os
+import pytest
+from pymongo import MongoClient
+
+def test_mongo_ping():
+    uri = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
+    client = MongoClient(uri)
+    assert client.admin.command("ping")["ok"] == 1.0
